6e73e409f6e967ef7367e1d0cf45f6ab6c3768b3
Iyamu Hope <Iyamuhope.nosa647@gmail.com>
2024-05-10 03:06:05 +0100

    feat: first commit

--- packages/cli/src/databases/migrations/postgresdb/1689942863739-dropConstraintOnSaveRequestLog.ts
+++ packages/cli/src/databases/migrations/postgresdb/1689942863739-dropConstraintOnSaveRequestLog.ts
@@ -5,14 +5,14 @@ export class dropConstraintOnSaveRequestLog1689942863739 implements ReversibleMi
 
 	public async up({ queryRunner, tablePrefix }: MigrationContext): Promise<void> {	
         await queryRunner.query(
-			`ALTER TABLE ${tablePrefix}save_request_log" DROP CONSTRAINT IF EXISTS "FK_9a3caef176a6bd386c9ca3b3799";`,
+			`ALTER TABLE ${tablePrefix}save_request_log DROP CONSTRAINT IF EXISTS "FK_9a3caef176a6bd386c9ca3b3799";`,
 		);
 	}
 
 	public async down({ queryRunner, tablePrefix }: MigrationContext): Promise<void> {
 		await queryRunner.query(
 			`
-				ALTER TABLE "${tablePrefix}save_request_log" ADD CONSTRAINT "FK_9a3caef176a6bd386c9ca3b3799" FOREIGN KEY ("workflowId") REFERENCES "workflow_entity"("id") ON DELETE NO ACTION ON UPDATE NO action
+				ALTER TABLE ${tablePrefix}save_request_log ADD CONSTRAINT "FK_9a3caef176a6bd386c9ca3b3799" FOREIGN KEY ("workflowId") REFERENCES "workflow_entity"("id") ON DELETE NO ACTION ON UPDATE NO action
 			`,
 		);
 	}
--- packages/cli/src/databases/migrations/postgresdb/1694091729096-addConstarinToSvaeRequestLog.ts
+++ packages/cli/src/databases/migrations/postgresdb/1694091729096-addConstarinToSvaeRequestLog.ts
@@ -6,14 +6,14 @@ export class addConstarinToSvaeRequestLog1694091729096 implements ReversibleMigr
 	public async up({ queryRunner, tablePrefix }: MigrationContext): Promise<void> {	
 		await queryRunner.query(
 			`
-				ALTER TABLE "${tablePrefix}save_request_log" ADD CONSTRAINT "FK_9a3caef176a6bd386c9ca3b3799" FOREIGN KEY ("workflowId") REFERENCES "workflow_entity"("id") ON DELETE NO ACTION ON UPDATE NO action
+				ALTER TABLE ${tablePrefix}save_request_log ADD CONSTRAINT "FK_9a3caef176a6bd386c9ca3b3799" FOREIGN KEY ("workflowId") REFERENCES "workflow_entity"("id") ON DELETE NO ACTION ON UPDATE NO action
 			`,
 		);
 	}
 
 	public async down({ queryRunner, tablePrefix }: MigrationContext): Promise<void> {
 		await queryRunner.query(
-			`ALTER TABLE ${tablePrefix}save_request_log" DROP CONSTRAINT IF EXISTS "FK_9a3caef176a6bd386c9ca3b3799";`,
+			`ALTER TABLE ${tablePrefix}save_request_log DROP CONSTRAINT IF EXISTS "FK_9a3caef176a6bd386c9ca3b3799";`,
 		);
 	}
 }
