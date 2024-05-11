165e3acf10f01a0aeb53c9af0201b423decb5079
Iyamu Hope <Iyamuhope.nosa647@gmail.com>
2024-05-11 21:04:27 +0100

    fix: otp setup view

--- packages/cli/src/databases/entities/User.ts
+++ packages/cli/src/databases/entities/User.ts
@@ -18,7 +18,7 @@ import { objectRetriever, lowerCaser } from '../utils/transformers';
 import { WithTimestamps, jsonColumnType } from './AbstractEntity';
 import type { IPersonalizationSurveyAnswers } from '@/Interfaces';
 import type { AuthIdentity } from './AuthIdentity';
-import type { SaveRequestLog } from './SaveRequestLog';
+// import type { SaveRequestLog } from './SaveRequestLog';
 import { ownerPermissions, memberPermissions, adminPermissions } from '@/permissions/roles';
 import { hasScope, type ScopeOptions, type Scope } from '@n8n/permissions';
 
@@ -111,8 +111,8 @@ export class User extends WithTimestamps implements IUser {
 	@Column({ nullable: true, type: String })
 	otpsecret?: string | null;
 
-	@OneToMany('SaveRequestLog', 'user')
-	saveRequestLogs: SaveRequestLog[];
+	// @OneToMany('SaveRequestLog', 'user')
+	// saveRequestLogs: SaveRequestLog[];
 
 	/**
 	 * Whether the user is pending setup completion.
