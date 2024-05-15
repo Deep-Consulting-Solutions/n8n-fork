374872924461ad94eabe0318a1b9ed32600fbd0a
Iyamu Hope <Iyamuhope.nosa647@gmail.com>
2024-05-15 16:27:54 +0100

    feat: added esa enums entities

--- packages/cli/src/databases/migrations/postgresdb/1899999999989-AddEnumsEntity.ts
+++ packages/cli/src/databases/migrations/postgresdb/1899999999989-AddEnumsEntity.ts
@@ -6,7 +6,7 @@ export class AddEnumsEntity1899999999989 implements ReversibleMigration {
     public async up({ queryRunner, tablePrefix }: MigrationContext): Promise<void> {
         await queryRunner.query(`
             CREATE TABLE "esa_enums" (
-                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "id" character varying NOT NULL,
                 "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
                 "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
                 "name" character varying NOT NULL,
@@ -15,12 +15,12 @@ export class AddEnumsEntity1899999999989 implements ReversibleMigration {
         `);
         await queryRunner.query(`
             CREATE TABLE "esa_enum_data" (
-                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "id" character varying NOT NULL,
                 "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
                 "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
                 "key" character varying NOT NULL,
                 "value" character varying NOT NULL,
-                "esaEnumId" uuid NOT NULL,
+                "esaEnumId" character varying NOT NULL,
                 CONSTRAINT "PK_b2b8a0b315f9aa7c1d0f8cb3b69" PRIMARY KEY ("id")
                 CONSTRAINT "FK_esa_enum_id" FOREIGN KEY ("esaEnumId") REFERENCES "esa_enums"("id") ON DELETE CASCADE
             )
